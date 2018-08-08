656d6a5ee9920b22b383f24a884ef7a4b89cb870
6weiqi <callmeliuweiqi@gmail.com>
2018-08-08 11:39:25 +0800

    1.去除秘钥信息 2.Release 0.0.4

--- toolbox/src/main/java/com/liuwq/base/activity/BaseActivity.java
+++ toolbox/src/main/java/com/liuwq/base/activity/BaseActivity.java
@@ -21,10 +21,11 @@ import io.reactivex.functions.Consumer;
 
 /**
  * 描述: Activity基类
- * 作者: su
- * 日期: 2017/9/21 14:34
+ *
+ * <p>作者: su
+ *
+ * <p>日期: 2017/9/21 14:34
  */
-
 public abstract class BaseActivity extends AppCompatActivity {
 
     protected Context mContext;
@@ -34,14 +35,14 @@ public abstract class BaseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mContext = this;
         // TODO: 2018/3/20 try a better way.
-//        overridePendingTransition(R.anim.slide_in_right, android.R.anim.fade_out);
+        //        overridePendingTransition(R.anim.slide_in_right, android.R.anim.fade_out);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         // TODO: 2018/3/20 try a better way.
-//        overridePendingTransition(android.R.anim.fade_in, R.anim.slide_out_right);
+        //        overridePendingTransition(android.R.anim.fade_in, R.anim.slide_out_right);
     }
 
     public View getContentView() {
@@ -53,62 +54,66 @@ public abstract class BaseActivity extends AppCompatActivity {
     }
 
     @SuppressLint("CheckResult")
-    protected void requestPermissions(@NonNull List<String> permissions,
-                                      @NonNull final PermissionCallback callback) {
+    protected void requestPermissions(
+            @NonNull List<String> permissions, @NonNull final PermissionCallback callback) {
         if ((permissions == null) || (permissions.size() == 0)) {
             return;
         }
 
-        final int count = permissions.size();   //count > 0 ;
+        final int count = permissions.size(); // count > 0 ;
         final List<Permission> permissionsGranted = new ArrayList<>();
         final List<Permission> permissionsDenied = new ArrayList<>();
 
         new RxPermissions(BaseActivity.this)
                 .requestEach(permissions.toArray(new String[count]))
-                .subscribe(new Consumer<com.tbruyelle.rxpermissions2.Permission>() {
-                    int index = 0;
+                .subscribe(
+                        new Consumer<com.tbruyelle.rxpermissions2.Permission>() {
+                            int index = 0;
 
-                    @Override
-                    public void accept(com.tbruyelle.rxpermissions2.Permission permission)
-                            throws Exception {
-                        index++;
+                            @Override
+                            public void accept(com.tbruyelle.rxpermissions2.Permission permission)
+                                    throws Exception {
+                                index++;
 
-                        if (permission.granted) {
-                            permissionsGranted.add(new Permission(permission.name, true,
-                                    false));
-                        } else {
-                            permissionsDenied.add(new Permission(permission.name, false,
-                                    permission.shouldShowRequestPermissionRationale));
+                                if (permission.granted) {
+                                    permissionsGranted.add(
+                                            new Permission(permission.name, true, false));
+                                } else {
+                                    permissionsDenied.add(
+                                            new Permission(
+                                                    permission.name,
+                                                    false,
+                                                    permission
+                                                            .shouldShowRequestPermissionRationale));
 
-                            //拒绝权限，并点击“不再询问”
-//                            if(permission.shouldShowRequestPermissionRationale){
-//                            }else {
-//                            }
-                        }
+                                    // 拒绝权限，并点击“不再询问”
+                                    //
+                                    // if(permission.shouldShowRequestPermissionRationale){
+                                    //                            }else {
+                                    //                            }
+                                }
 
-                        if (index == count) {
-                            //所有权限请求完毕
-                            int countGranted = permissionsGranted.size();
+                                if (index == count) {
+                                    // 所有权限请求完毕
+                                    int countGranted = permissionsGranted.size();
 
-                            if (countGranted == count) {
-                                //所有权限都成功获取被授予；
-                                callback.onPermissionGranted(permissionsGranted, true);
-                            } else if (countGranted == 0) {
-                                callback.onPermissionDenied(permissionsDenied, true);
-                            } else {
-                                callback.onPermissionGranted(permissionsGranted, false);
-                                callback.onPermissionDenied(permissionsDenied, false);
+                                    if (countGranted == count) {
+                                        // 所有权限都成功获取被授予；
+                                        callback.onPermissionGranted(permissionsGranted, true);
+                                    } else if (countGranted == 0) {
+                                        callback.onPermissionDenied(permissionsDenied, true);
+                                    } else {
+                                        callback.onPermissionGranted(permissionsGranted, false);
+                                        callback.onPermissionDenied(permissionsDenied, false);
+                                    }
+                                }
                             }
-                        }
-                    }
-                });
+                        });
     }
-
 
     public interface PermissionCallback {
         void onPermissionGranted(List<Permission> permissions, boolean allGranted);
 
         void onPermissionDenied(List<Permission> permissions, boolean allDenied);
     }
-
 }
