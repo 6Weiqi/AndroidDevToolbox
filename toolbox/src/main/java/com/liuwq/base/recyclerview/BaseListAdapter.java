532cfb3d1075ecaa43a7966fc1cb689c218032d0
Weiqi <callmeliuweiqi@gmail.com>
2018-09-23 17:30:05 +0800

    1.BaseListAdapter 改为 diff 刷新 2.Update Java compile version to 1.8

--- toolbox/src/main/java/com/liuwq/base/recyclerview/BaseListAdapter.java
+++ toolbox/src/main/java/com/liuwq/base/recyclerview/BaseListAdapter.java
@@ -3,28 +3,37 @@ package com.liuwq.base.recyclerview;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.util.ObjectsCompat;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
- * 描述: 一般 RecyclerView 列表适配器基类
- * 作者: su
+ * 描述: 一般 RecyclerView 列表适配器基类 <br>
+ * 作者: su <br>
  * 日期: 2017/10/31 15:39
  */
+public abstract class BaseListAdapter<E, ViewHolder extends RecyclerView.ViewHolder>
+        extends RecyclerView.Adapter<ViewHolder> {
 
-public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
-        extends RecyclerView.Adapter<VH> {
-    public static final int POSITION_NONE = -1;
     private Context mContext;
-    private List<E> mDataList;
+    @NonNull private List<E> mDataList = Collections.emptyList();
     private RecyclerView mRecyclerView;
     private LayoutInflater mLayoutInflater;
-    @Nullable
-    protected OnItemClickListener<E> mItemClickListener;
+    @Nullable protected OnItemClickListener<E> mItemClickListener;
+    private Disposable mSubscribe;
 
     public interface OnItemClickListener<E> {
         void onItemClick(View itemView, int pos, E data);
@@ -49,8 +58,10 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
     @Override
     public void onDetachedFromRecyclerView(RecyclerView recyclerView) {
         super.onDetachedFromRecyclerView(recyclerView);
-
         this.mRecyclerView = null;
+        if (mSubscribe != null && !mSubscribe.isDisposed()) {
+            mSubscribe.dispose();
+        }
     }
 
     @Override
@@ -66,10 +77,8 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
         return getData().size();
     }
 
+    @NonNull
     public List<E> getData() {
-        if (mDataList == null) {
-            mDataList = new ArrayList<>();
-        }
         return mDataList;
     }
 
@@ -80,35 +89,64 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
         return getData().get(position);
     }
 
-    public void setData(@Nullable List<E> dataList) {
-        if (dataList == null || dataList.isEmpty()) {
-            return;
-        }
+    protected void setList(@NonNull List<E> list) {
+        checkNotNull(list);
+        mDataList = list;
+    }
 
-        getData().clear();
-        getData().addAll(dataList);
-        notifyDataSetChanged();
+    public void setData(@NonNull final List<E> dataList) {
+        checkNotNull(dataList);
+        mSubscribe =
+                Single.fromCallable(
+                                () -> {
+                                    BaseDiffCallback diffCallback =
+                                            new BaseDiffCallback(mDataList, dataList);
+                                    return DiffUtil.calculateDiff(diffCallback);
+                                })
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                diffResult -> {
+                                    setList(dataList);
+                                    diffResult.dispatchUpdatesTo(this);
+                                });
+        //        if (dataList.isEmpty()) {
+        //            return;
+        //        }
+        //
+        //        getData().clear();
+        //        getData().addAll(dataList);
+        //        notifyDataSetChanged();
+        //
+        //        if (autoNotify()) {
+        //            notifyDataSetChanged();
+        //            int prevSize = getData().size();
+        //            //            notifyItemRangeRemoved(0, prevSize);
+        //            //            notifyItemRangeInserted(0, dataList.size());
+        //        }
+    }
 
-        if (autoNotify()) {
-            notifyDataSetChanged();
-            int prevSize = getData().size();
-            //            notifyItemRangeRemoved(0, prevSize);
-            //            notifyItemRangeInserted(0, dataList.size());
-        }
+    protected void addList(@NonNull List<E> list) {
+        checkNotNull(list);
+        mDataList.addAll(list);
     }
 
     public void addData(@NonNull List<E> dataList) {
-        if (dataList.isEmpty()) {
-            return;
-        }
-        int prevSize = getData().size();
-        getData().addAll(dataList);
-        notifyDataSetChanged();
-
-        if (autoNotify()) {
-            //            notifyDataSetChanged();
-            notifyItemRangeInserted(prevSize, dataList.size());
-        }
+        addList(dataList);
+        int positionStart = mDataList.size();
+        int insertedCount = dataList.size();
+        notifyItemRangeInserted(positionStart, insertedCount);
+        //        if (dataList.isEmpty()) {
+        //            return;
+        //        }
+        //        int prevSize = getData().size();
+        //        getData().addAll(dataList);
+        //        notifyDataSetChanged();
+        //
+        //        if (autoNotify()) {
+        //            //            notifyDataSetChanged();
+        //            notifyItemRangeInserted(prevSize, dataList.size());
+        //        }
     }
 
     public boolean addData(@NonNull E data) {
@@ -116,31 +154,36 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
     }
 
     public boolean addData(@NonNull E data, int position) {
+        checkNotNull(data);
         if (!isLegalPositionForAdding(position)) {
             return false;
         }
-
-        getData().add(position, data);
-
-        if (autoNotify() && isRecyclerViewAttached()) {
-            //            notifyItemInserted(position);
-            //            scrollToPosition(position);
-            notifyDataSetChanged();
-        }
+        mDataList.add(position, data);
+        notifyItemInserted(position);
+        //        getData().add(position, data);
+        //
+        //        if (autoNotify() && isRecyclerViewAttached()) {
+        //            //            notifyItemInserted(position);
+        //            //            scrollToPosition(position);
+        //            notifyDataSetChanged();
+        //        }
         return true;
     }
 
     public boolean updateData(@NonNull E data, int position) {
+        checkNotNull(data);
         if (!isLegalPositionForGetting(position)) {
             return false;
         }
+        mDataList.set(position, data);
+        notifyItemChanged(position);
 
-        getData().set(position, data);
-
-        if (autoNotify() && isRecyclerViewAttached()) {
-            notifyItemChanged(position);
-            scrollToPosition(position);
-        }
+        //        getData().set(position, data);
+        //
+        //        if (autoNotify() && isRecyclerViewAttached()) {
+        //            notifyItemChanged(position);
+        //            scrollToPosition(position);
+        //        }
 
         return true;
     }
@@ -153,22 +196,22 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
         if (!isLegalPositionForGetting(position)) {
             return false;
         }
-
-        getData().remove(position);
-
-        if (autoNotify() && isRecyclerViewAttached()) {
-            //            notifyItemRemoved(position);
-            //            notifyItemRangeChanged(position, getCount() - position);
-            // TODO: 2017/11/8
-            notifyDataSetChanged();
-        }
+        mDataList.remove(position);
+        notifyItemRemoved(position);
+        //        getData().remove(position);
+        //
+        //        if (autoNotify() && isRecyclerViewAttached()) {
+        //            //            notifyItemRemoved(position);
+        //            //            notifyItemRangeChanged(position, getCount() - position);
+        //            // TODO: 2017/11/8
+        //            notifyDataSetChanged();
+        //        }
 
         return true;
     }
 
     public boolean removeData(int positionStart, int itemCount) {
-        if (!isLegalPositionForGetting(positionStart)
-                || (positionStart + itemCount) > getCount()) {
+        if (!isLegalPositionForGetting(positionStart) || (positionStart + itemCount) > getCount()) {
             return false;
         }
 
@@ -177,13 +220,14 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
         for (int i = itemCount - 1; i >= 0; i--) {
             removeList.add(getData(positionStart + i));
         }
-
-        getData().removeAll(removeList);
-
-        if (autoNotify() && isRecyclerViewAttached()) {
-            //            notifyItemRangeChanged(positionStart, itemCount);
-            notifyDataSetChanged();
-        }
+        mDataList.removeAll(removeList);
+        notifyItemRangeRemoved(positionStart, itemCount);
+        //        getData().removeAll(removeList);
+        //
+        //        if (autoNotify() && isRecyclerViewAttached()) {
+        //            //            notifyItemRangeChanged(positionStart, itemCount);
+        //            notifyDataSetChanged();
+        //        }
 
         return true;
     }
@@ -219,16 +263,19 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
         }
 
         int count = getItemCount();
-        getData().clear();
-
-        if (autoNotify() && isRecyclerViewAttached()) {
-            notifyItemRangeRemoved(0, count);
-        }
+        mDataList.clear();
+        notifyItemRangeRemoved(0, count);
+        //        getData().clear();
+        //
+        //        if (autoNotify() && isRecyclerViewAttached()) {
+        //            notifyItemRangeRemoved(0, count);
+        //        }
     }
 
     public int getDataPosition(@NonNull E data) {
+        checkNotNull(data);
         if (isEmpty()) {
-            return POSITION_NONE;
+            return RecyclerView.NO_POSITION;
         }
 
         int n = getItemCount();
@@ -238,7 +285,8 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
                 return i;
             }
         }
-        return POSITION_NONE;
+
+        return RecyclerView.NO_POSITION;
     }
 
     public boolean isEmpty() {
@@ -292,5 +340,19 @@ public abstract class BaseListAdapter<E, VH extends RecyclerView.ViewHolder>
             mLayoutInflater = LayoutInflater.from(mContext);
         }
         return mLayoutInflater;
+    }
+
+    class BaseDiffCallback extends DiffCallback<E> {
+
+        public BaseDiffCallback(List<E> oldList, List<E> newList) {
+            super(oldList, newList);
+        }
+
+        @Override
+        public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+            E oldItem = mOldList.get(oldItemPosition);
+            E newItem = mNewList.get(newItemPosition);
+            return ObjectsCompat.equals(oldItem, newItem);
+        }
     }
 }
