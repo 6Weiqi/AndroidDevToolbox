e216277d9fa9245fcd57a9ea61874a35d307a9d0
Weiqi <callmeliuweiqi@gmail.com>
2018-09-29 22:45:41 +0800

    代码调整

--- toolbox/src/main/java/com/liuwq/base/databinding/BaseRequestViewModel.java
+++ toolbox/src/main/java/com/liuwq/base/databinding/BaseRequestViewModel.java
@@ -1,7 +1,6 @@
 package com.liuwq.base.databinding;
 
 import android.app.Application;
-import android.arch.lifecycle.MutableLiveData;
 import android.databinding.ObservableBoolean;
 import android.support.annotation.NonNull;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -9,31 +8,25 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import io.reactivex.disposables.Disposable;
 
 /**
+ *
+ *
+ * <pre>
  * 描述:
  * 作者: su
  * 日期: 2017/11/1 17:44
  */
-
 public abstract class BaseRequestViewModel extends BaseViewModel {
 
-    /**
-     * 是否处于刷新中
-     */
+    /** 是否处于刷新中 */
     public ObservableBoolean pObRefreshing = new ObservableBoolean(false);
 
-    /**
-     * 错误信息
-     */
-    public MutableLiveData<String> pMldError = new MutableLiveData<>();
+    /** 错误信息 */
+    public SingleLiveEvent<String> pErrorEvent = new SingleLiveEvent<>();
 
-    /**
-     * 成功信息
-     */
-    public MutableLiveData<String> pMldSuccess = new MutableLiveData<>();
+    /** 成功信息 */
+    public SingleLiveEvent<String> pSuccessEvent = new SingleLiveEvent<>();
 
-    /**
-     * 用于取消订阅
-     */
+    /** 用于取消订阅 */
     protected Disposable mDisposable;
 
     public BaseRequestViewModel(@NonNull Application application) {
@@ -45,21 +38,16 @@ public abstract class BaseRequestViewModel extends BaseViewModel {
      *
      * @see ViewBindings#setOnRefreshListener(SwipeRefreshLayout, BaseRequestViewModel)
      */
-    public void onRefresh() {
-    }
+    public void onRefresh() {}
 
-    /**
-     * 取消上下游间的订阅关系，上游依然发送事件，下游接收不到事件
-     */
+    /** 取消上下游间的订阅关系，上游依然发送事件，下游接收不到事件 */
     public void dispose() {
         if (mDisposable != null && !mDisposable.isDisposed()) {
             mDisposable.dispose();
         }
     }
 
-    /**
-     * 包括一个取消订阅的操作
-     */
+    /** 包括一个取消订阅的操作 */
     @Override
     public void onLifecycleOwnerDestroy() {
         super.onLifecycleOwnerDestroy();
